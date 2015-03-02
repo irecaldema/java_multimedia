e9f77b54331c2a6a07c6a172ba7d876a4028af11
zubiri <irecaldema14dw@ikzubirimanteo.com>
2015-03-02 13:24:06 +0000

    123

--- src/com/zubiri/multiteca/Main.java
+++ src/com/zubiri/multiteca/Main.java
@@ -1,10 +1,77 @@
 package com.zubiri.multiteca;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.io.FileNotFoundException;
+
 public final class Main {
 
 	public static void main(String[] args) {
+	
+		int seleccion=0;
+		int error=0;
+		Scanner sc = new Scanner(System.in);
+		Productos productos = null;
 		
+		if (args.length == 0) {
+			System.out.println("El número de argumentos no es válido:");
+			System.out.println("java main <fichero_distribuidores>");
+			System.exit(-1);
+		} 
+		
+    	//Leemos la lista de distribuidores del fichero.
+    	do{
+	    	try {
+	    		if (error==0){
+					Distribuidores.leerDistribuidores(args[0]);
+	    		}	
+				else{	
+					Distribuidores.leerDistribuidores("../"+sc.next());//una carpeta hacia arriba de src/
+	    		}
+	    	} catch (FileNotFoundException errorifico) {
+	    		System.out.println("El archivo de distribuidores no es el correcto");
+	    		error=1;
+	    	}	
+		}while(Distribuidores.getDistribuidores().size()==0);	
 
+		do {
+			try{
+				//Visualiza por terminal (stdout) las opciones del men�
+				System.out.println("Mostrar distribuidores---------------------------1");
+				System.out.println("Solicitar productos------------------------------2");
+				System.out.println("Mostrar productos--------------------------------3");
+				System.out.println("SALIR DEL PROGRAMA-------------------------------4");
+				   
+				seleccion = sc.nextInt();
+				switch (seleccion) {
+					
+					case 1: //Mostrar distribuidores
+						Distribuidores.mostrarDistribuidores();
+						break;
+						
+					case 2: //Solicitar productos
+						productos = new Productos(sc);
+						break;
+					case 3: //Mostrar productos
+						if (productos == null) {
+							System.out.println("No es posible mostrar productos sin haber sido solicitados");
+						}
+						productos.mostrarProductos();
+						break;
+					case 4: //Salimos
+						break;
+					default:
+						System.out.println("No ha insertado la opción correcta");
+				}
+			}catch(InputMismatchException e){
+				System.out.println("No ha insertado la opción correcta");
+				sc.nextLine();
+			}
+		} while (seleccion != 4);
+		System.out.println("Gracias por usar nuestro programa. Que tenga un buen día!");
+		sc.close();
 	}
 
 }
